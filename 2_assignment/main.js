8532261130174696fc63b5c7706b1725e2c4edac
silmu <svetlana.raitina@gmail.com>
2022-03-13 13:17:32 +0200

    Added search functionality, display all items and add new item

--- 2_assignment/main.js
+++ 2_assignment/main.js
@@ -5,20 +5,21 @@ class FetchWrapper {
     this.baseURL = baseURL;
   }
 
-  async get(endpoint) {
-    const response = await fetch(baseURL + '/' + endpoint);
-    const data = await response.json();
-    console.log(data);
-  }
-  // get(endpoint) {
-  //   return fetch(this.baseURL + endpoint).then((response) => response.json());
+  // async get(endpoint) {
+  //   const response = await fetch(baseURL + '/' + endpoint);
+  //   const data = await response.json();
+  //   console.log(data);
   // }
+  get(endpoint) {
+    return fetch(this.baseURL + endpoint).then((response) => response.json());
+  }
 
   put(endpoint, body) {
     return this._send('put', endpoint, body);
   }
 
   post(endpoint, body) {
+    console.log('Post sent: ' + body);
     return this._send('post', endpoint, body);
   }
 
@@ -42,8 +43,92 @@ class FetchWrapper {
 }
 
 //%7b is { %7d is }. The code between is random id
+//Current endpoint: %7b0fa8c6aa-bac3-4cd0%7d
+//{0fa8c6aa-bac3-4cd0}
 const API = new FetchWrapper(
-  'https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/%7b0fa8c6aa-bac3-4cd0%7d'
+  'https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/'
 );
 
-API.get();
+const foodList = document.querySelector('.food-list');
+
+//Fetch all food items
+const displayFoodList = () => {
+  API.get('{0fa8c6aa-bac3-4cd0}').then((data) => {
+    data.documents.forEach((item) => {
+      const li = document.createElement('li');
+      li.textContent = `${item.fields.name.stringValue.toUpperCase()} - carbs: ${
+        item.fields.carbs.integerValue
+      }, fat: ${item.fields.fat.integerValue}, protein: ${
+        item.fields.protein.integerValue
+      }, calories: ${item.fields.calories.integerValue}`;
+      foodList.appendChild(li);
+    });
+  });
+};
+
+//Display food items list on load
+displayFoodList();
+
+//Search for food item oin the existing food list
+const inputSearch = document.querySelector('#input-search');
+
+const searchForFood = (foodName) => {
+  const foodListItems = document.querySelectorAll('li');
+  foodListItems.forEach((item) => {
+    if (item.textContent.includes(foodName.toUpperCase())) {
+      item.style.display = 'inline-block';
+      console.log(`${item.textContent} includes ${foodName}`);
+    } else {
+      item.style.display = 'none';
+    }
+  });
+  console.log(foodName);
+};
+
+inputSearch.addEventListener('keyup', () => {
+  searchForFood(inputSearch.value);
+});
+
+//Post new food to server with name, carbs, protein, fat and calories info
+const postNewFood = (carbs, fat, protein, name, calories) => {
+  API.post('{0fa8c6aa-bac3-4cd0}', {
+    fields: {
+      name: {
+        stringValue: name,
+      },
+      carbs: {
+        integerValue: carbs,
+      },
+      protein: {
+        integerValue: protein,
+      },
+      fat: {
+        integerValue: fat,
+      },
+      calories: {
+        integerValue: calories,
+      },
+    },
+  });
+};
+
+//Add new food item
+const btnAddNewFood = document.querySelector('#btn-add-new-food');
+
+btnAddNewFood.addEventListener('click', (e) => {
+  e.preventDefault();
+  //Select html elements for Add new food form
+  const inputName = document
+    .querySelector('#input-new-food')
+    .value.toLowerCase();
+  const inputCarbs = document.querySelector('#input-carbs').value;
+  const inputProtein = document.querySelector('#input-protein').value;
+  const inputFat = document.querySelector('#input-fat').value;
+  const inputCals = document.querySelector('#input-calories').value;
+  const output = document.querySelector('.added-food');
+  console.log(inputCarbs, inputFat, inputProtein, inputName, inputCals);
+  //Display new food that is added under the form
+  output.textContent = `Added ${inputName} (carbs: ${inputCarbs}, protein: ${inputProtein}, fat: ${inputFat}, calories: ${inputCals})`;
+  //Call post function to send new food info
+  postNewFood(inputCarbs, inputFat, inputProtein, inputName, inputCals);
+});
